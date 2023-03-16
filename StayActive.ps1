40f865648750e8fb6d12dfc24d2f715dd65e5bb3
VirtualOx <37578273+virtualox@users.noreply.github.com>
2023-03-16 15:04:55 +0100

    Create StayActive.ps1
    This script will move the mouse cursor by 1 pixel every 4 minutes to simulate activity. To stop the script, close the PowerShell window or press Ctrl+C in the PowerShell console.

---
+++ StayActive.ps1
@@ -0,0 +1,19 @@
+# StayActive.ps1
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Mouse {
+    [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+    public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+}
+"@ -Language CSharp
+
+$MOUSEEVENTF_MOVE = 0x0001
+
+while ($true) {
+    [Mouse]::mouse_event($MOUSEEVENTF_MOVE, 1, 1, 0, 0)
+    Start-Sleep -Seconds 1
+    [Mouse]::mouse_event($MOUSEEVENTF_MOVE, 4294967295, 4294967295, 0, 0)
+    Start-Sleep -Minutes 4
+}
